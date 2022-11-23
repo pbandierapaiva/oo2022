4dbdfa73f13d2e133a32aca6a29e673dcbc2ddec
PAULO BANDIERA PAIVA <paiva@unifesp.br>
2022-11-23 09:48:22 -0300

    23/11/2022 - NodeJS + ExpressJS - backend de CEP

---
+++ cepapi/index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { exec } = require('child_process');
+
+
+app.get('/', (req, res) => {
+  res.send('API para acesso ao CEP')
+})
+
+app.get('/cep/:numero', (req, res) => {
+
+	let resultado = {"STATUS":"Iniciando..."}
+	
+	strBusca = `grep ^${req.params.numero}   /home/pub/ceps.txt`
+
+	exec( strBusca, (err, stdout, stderr) => {
+		console.log("Executando....")
+		if (err) {
+			console.log(err)
+			resultado["STATUS"]="ERRO"
+			res.json(resultado)
+			return
+			}
+		
+		linhas = stdout.split("\n")
+		let listaend = []
+		for(let i=0; i< linhas.length-1; i++) {
+			let logra = {}
+			let partes =linhas[i].split("\t")
+			logra["CEP"] = partes[0]
+			logra["Cidade"] = partes[1]
+			logra["Bairro"] = partes[2]
+			logra["Rua"] = partes[3].split(" - ")[0]
+			listaend.push( logra )
+		}
+		resultado["STATUS"]="OK"
+		resultado["Dado"] = listaend
+		res.json(resultado)
+	})
+})
+
+app.get('/cep/busca/:nome',  (req, res) => {
+
+	console.log(req.params.nome)
+	let resultado = {"STATUS":"Iniciando..."}
+	
+	strBusca = `grep -i ${req.params.nome}   /home/pub/ceps.txt`
+
+	exec( strBusca, (err, stdout, stderr) => {
+		console.log("Executando....")
+		if (err) {
+			console.log(err)
+			resultado["STATUS"]="ERRO"
+			res.json(resultado)
+			return
+			}
+		linhas = stdout.split("\n")
+		let listaend = []
+		for(let i=0; i< linhas.length-1; i++) {
+			let logra = {}
+			let partes =linhas[i].split("\t")
+			logra["CEP"] = partes[0]
+			logra["Cidade"] = partes[1]
+			logra["Bairro"] = partes[2]
+			logra["Rua"] = partes[3].split(" - ")[0]
+			listaend.push( logra )
+		}
+		resultado["STATUS"]="OK"
+		resultado["Dado"] = listaend
+		res.json(resultado)
+	})
+})
+
+
+app.listen(port, () => {
+  console.log(`Aceitando requisições na porta ${port}`)
+})
